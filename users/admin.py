9088b9e4f9655617259fc79fc9eb290440f681f2
E-ugine <agollaeugine@gmail.com>
2025-09-14 13:04:15 +0300

    chore: Create user model[full auth]

---
+++ users/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+@admin.register(User)
+class CustomUserSdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields":("role",)}),
+    )
